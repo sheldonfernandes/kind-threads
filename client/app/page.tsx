e02ea9c76416d9442c5017c7c8af454818e723fa
Sheldon Fernandes <sheldon3@us.ibm.com>
2024-10-02 16:13:43 -0400

    feat: user posts (simulated data)

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,7 +1,14 @@
 "use client";
 
+import InfiniteScrollPosts from "./post/page";
+
 export const Dashboard = () => {
-  return <div>Statistics Coming Soon</div>;
+  return <div style={{backgroundColor: '#F2F2F0'}}>
+    
+    Statistics Coming Soon 
+  <InfiniteScrollPosts />
+
+  </div>;
 };
 
 export default Dashboard;
