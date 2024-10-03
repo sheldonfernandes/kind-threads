a30812d1daf9ace0619b45680b91a871a159dd74
Purva <Purva.Naik@ibm.com>
2024-10-03 16:47:35 +0530

    Admin Menu on Header

--- client/src/constants/endpoints.constant.ts
+++ client/src/constants/endpoints.constant.ts
@@ -1,6 +1,7 @@
 export const EndpointConst = {
     DASHBOARD_PAGE : "/",
     LOGIN_PAGE : "/login",
+    ADMIN_ORG_LIST_PAGE : "/admin/listoforganization",
     DONATION_PAGE : "/donation",
     DONATION_DETAILS_PAGE : "/donation-details"
 }
